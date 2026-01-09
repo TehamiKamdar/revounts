26d912d97778c28b5bff19f507682fcc71a7f18b
TehamiKamdar <tehamikamdar15@gmail.com>
2026-01-09 18:00:13 +0500

    Reviews Page

---
+++ resources/views/pages/reviews.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="hero-section text-center">
+    <img src="{{ asset('assets/images/banner_1560x480.png') }}" alt="" style="padding-top: 0;" class="hero-img">
+</div>
+@endsection
